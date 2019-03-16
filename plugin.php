e53f9bfac6ab919cb92c2007797d760cde4f12f6
Jannik Schäfer <me@jschaefer.io>
2019-03-16 18:53:18 +0100

    Update license information and plugin link

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP-Valkyrie
  * Description: Abstraction layer for the WordPress ecosystem
- * Plugin URI: https://github.com/jschaefer-io/wp-core
+ * Plugin URI: https://github.com/wp-valkyrie/valkyrie
  * Author: Jannik Schäfer <me@jschaefer.io>
  * Author URI: https://jschaefer.io/
  * Version: 1.0
