9c27b85334e4628069059125f84116887a5cf420
Jannik Schäfer <me@jschaefer.io>
2019-05-29 12:54:58 +0200

    fix slash problem from grouped fields

--- Valkyrie/Form/GroupDispatcher.php
+++ Valkyrie/Form/GroupDispatcher.php
@@ -40,7 +40,10 @@ class GroupDispatcher extends Dispatcher{
         $name = $this->cleanName($name);
         $post = $this->getPost();
         if (isset($post[$name])) {
-            return $post[$name];
+            $data = $post[$name];
+            if (is_string($data)){
+                $data = stripslashes_deep($data);
+            }
         }
         return null;
     }
