83d587dd1b0434d89161a90e95690d09467038c7
Jannik Schäfer <me@jschaefer.io>
2019-08-08 14:23:11 +0200

    Add some reflection support to the main module/component pipelines

--- Valkyrie/Pipeline.php
+++ Valkyrie/Pipeline.php
@@ -26,17 +26,37 @@ abstract class Pipeline{
      * @throws \Exception throws an exception if the pipeline does not provide a method with the given name
      */
     public final function access(string $name){
-        if (!method_exists($this, $name)) {
-            throw new \Exception('Pipeline Method ' . $name . ' does not exist.');
+        if (!method_exists($this, $name) || !(new \ReflectionMethod($this, $name))->isPublic()) {
+            throw new \Exception('Pipeline Method ' . $name . ' does not exist, or is not public.');
         }
         return call_user_func_array([$this, $name], array_slice(func_get_args(), 1));
+    }
+
+    /**
+     * Returns a list of all methods from the current pipeline
+     * @return array
+     */
+    public final function getPipelineMethods(){
+        try {
+            $reflection = new \ReflectionClass($this);
+            $methods = $reflection->getMethods(\ReflectionMethod::IS_PUBLIC);
+            $methods = array_filter($methods, function(\ReflectionMethod $method){
+               return $method->class !== self::class;
+            });
+            $methods = array_map(function(\ReflectionMethod $method){
+                return $method->getName();
+            }, $methods);
+            return $methods;
+        } catch (\ReflectionException $e) {
+            return [];
+        }
     }
 
     /**
      * Returns the parent module
      * @return Module
      */
-    public final function getModule(): Module{
+    protected final function getModule(): Module{
         return $this->module;
     }
 }
